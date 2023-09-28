53aee1d0847fd16c360da5f0f18046a3ffd48cbd
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-28 12:21:48 +0000

    10 loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* For...of Loops */
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+
+  return arrayEnd;
+}
