cf75860c30511fc0fe0eafe53a10058b967821b7
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-28 15:32:10 +0000

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+/*  EVCar */
+/* eslint-disable */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
+
+	cloneCar() {
+		return new Car();
+	}
+}
