451daba6b607dfe8d7c0e83d3633d3a15fce44f8
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-28 12:00:05 +0000

    8 Get Budget Current Year

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+/* No need to create empty objects before adding in properties */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
