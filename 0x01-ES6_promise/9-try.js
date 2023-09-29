ff24d17826b82c5865d7c0cbebb2fbb75093dbd8
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-29 15:38:30 +0000

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* Throw an error */
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
