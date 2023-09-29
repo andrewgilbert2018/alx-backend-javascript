1e3934117884823a46bf9a2aff7f2ddd2a2708a5
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-29 15:41:37 +0000

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+/* Await / Async */
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const prom1 = await uploadPhoto();
+    const prom2 = await createUser();
+
+    return ({ photo: prom1, user: prom2 });
+  } catch (error) {
+    return ({ photo: null, user: null });
+  }
+}
