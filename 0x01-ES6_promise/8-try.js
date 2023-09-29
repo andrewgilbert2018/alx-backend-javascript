5745935a59bf88e1a97a78fd805fac5e6e424864
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-29 15:33:14 +0000

    8-try.js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* Throw error / try catch */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
