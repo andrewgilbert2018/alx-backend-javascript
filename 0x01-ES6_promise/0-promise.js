9d377b4ba5f27165817faf2b6fb22688f16eff5e
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-09-29 15:52:47 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,5 @@
-/*Keep every promise you make and only make promises you can keep */
+/*Keep every promise you make and 
+ * only make promises you can keep */
 /* eslint-disable */
 export default function getResponseFromAPI() {
   return (new Promise((resolve, reject) => {}));
