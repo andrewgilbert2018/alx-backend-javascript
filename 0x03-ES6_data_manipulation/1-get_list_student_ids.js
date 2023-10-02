9c08bc93bd872779346afb1351b3ef9ee5c15215
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-02 09:57:43 +0000

    1-get_list_student_ids.js

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/* More mapping */
+const getListStudentIds = (myArray) => {
+  if (!Array.isArray(myArray)) {
+    return [];
+  }
+  return myArray.map((studentId) => studentId.id);
+};
+
+export default getListStudentIds;
