a88d963157519d792b44234d296953f349eecf12
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-02 10:23:20 +0000

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,17 @@
+/* Map data structure */
+const groceriesList = () => {
+  const res = new Map();
+  const objetos = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+  for (const key of Object.keys(objetos)) {
+    res.set(key, objetos[key]);
+  }
+  return res;
+};
+
+export default groceriesList;
