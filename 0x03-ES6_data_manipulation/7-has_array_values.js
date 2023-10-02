d22df3840cc790cf24f7119c38f1d4b9fe1a1b49
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-02 10:17:19 +0000

    7-has_array_values.js

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+/* More set data structure */
+const hasValuesFromArray = (set, array) => {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default hasValuesFromArray;
