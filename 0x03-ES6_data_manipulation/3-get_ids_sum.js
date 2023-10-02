b0b4194d3894c0753786ed7f345a6ebeeb15de11
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-02 10:04:10 +0000

    3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+/* Reduce */
+const getStudentIdsSum = (students) => {
+  const id = students.reduce(
+    (acc, value) => acc + value.id, 0,
+  );
+  return id;
+};
+
+export default getStudentIdsSum;
