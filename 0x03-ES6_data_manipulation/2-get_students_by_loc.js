bc76031d98100931ec14543dd1564a9a12620393
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-10-02 10:00:28 +0000

    2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+/* Filter */
+const getStudentsByLocation = (students, city) => {
+  const arratyObjec = students.filter((funtion) => funtion.location === city);
+  return arratyObjec;
+};
+
+export default getStudentsByLocation;
